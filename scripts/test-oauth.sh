22368275e42535e9d5ef7322ef00f8772824aae9
Ermias <ermiasg@kth.se>
2024-12-05 20:23:08 +0100

    get groups

--- scripts/test-oauth.sh
+++ scripts/test-oauth.sh
@@ -3,7 +3,7 @@
 CLIENT_ID=${1:-"hopsworks-app"}
 CLIENT_SECRET=${2:-"da9d22be-dc88-457f-ac03-33789699e140"}
 
-curl -s "http://localhost:8080/realms/hopsworks/.well-known/openid-configuration" -o /tmp/openid-configuration.json
+curl -s "http://keycloak.hopsworks.svc.cluster.local:8080/realms/hopsworks/.well-known/openid-configuration" -o /tmp/openid-configuration.json
 
 AUTH_ENDPOINT=$(jq -r '.authorization_endpoint' /tmp/openid-configuration.json)
 TOKEN_ENDPOINT=$(jq -r '.token_endpoint' /tmp/openid-configuration.json)
@@ -11,10 +11,24 @@ USERINFO_ENDPOINT=$(jq -r '.userinfo_endpoint' /tmp/openid-configuration.json)
 
 # echo "$AUTH_ENDPOINT?response_type=code&client_id=$CLIENT_ID&client_secret=$CLIENT_SECRET&scope=openid+profile+email&redirect_uri=https://localhost:8181/callback"
 
-curl \
+curl -s \
   -d "client_id=$CLIENT_ID" \
   -d "client_secret=$CLIENT_SECRET" \
+  -d "scope=openid+profile+email" \
   -d "username=admin@hopsworks.ai" \
-  -d "password=admin" \
+  -d "password=adminpw" \
   -d "grant_type=password" \
-  "$TOKEN_ENDPOINT"
+  "$TOKEN_ENDPOINT" -o /tmp/oauth-token.json
+
+has_error=$(jq 'has("error")' /tmp/oauth-token.json)
+if [ "$has_error" == true ]; then 
+  jq -r '.error_description' /tmp/oauth-token.json
+  rm /tmp/oauth-token.json
+  exit 1
+fi
+
+ACCESS_TOKEN=$(jq -r '.access_token' /tmp/oauth-token.json)
+
+curl -s \
+  -H "Authorization: bearer $ACCESS_TOKEN" \
+  "$USERINFO_ENDPOINT" | jq
